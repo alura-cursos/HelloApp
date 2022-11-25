613e8fe52ffc7b3b83dde5d0238676fa0f29417c
Junior <35709152+git-jr@users.noreply.github.com>
2022-11-25 09:47:53 -0300

    3.1

--- app/src/main/java/br/com/alura/helloapp/database/ContatoDao.kt
+++ app/src/main/java/br/com/alura/helloapp/database/ContatoDao.kt
@@ -13,4 +13,7 @@ interface ContatoDao {
 
     @Query("SELECT * FROM Contato")
     suspend fun buscaTodos(): List<Contato>
+
+    @Query("SELECT * FROM Contato WHERE id = :id")
+    suspend fun buscaPorId(id: Long): Contato?
 }
--- app/src/main/java/br/com/alura/helloapp/ui/details/DetalhesContatoViewlModel.kt
+++ app/src/main/java/br/com/alura/helloapp/ui/details/DetalhesContatoViewlModel.kt
@@ -3,15 +3,19 @@ package br.com.alura.helloapp.ui.details
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import br.com.alura.helloapp.database.ContatoDao
 import br.com.alura.helloapp.util.ID_CONTATO
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-
-class DetalhesContatoViewlModel(
-    savedStateHandle: SavedStateHandle
+@HiltViewModel
+class DetalhesContatoViewlModel @Inject constructor(
+    savedStateHandle: SavedStateHandle,
+    private val contatoDao: ContatoDao
 ) : ViewModel() {
 
     private val idContato = savedStateHandle.get<Long>(ID_CONTATO)
@@ -27,6 +31,23 @@ class DetalhesContatoViewlModel(
     }
 
     private suspend fun carregaContato() {
+        idContato?.let {
+            val contato = contatoDao.buscaPorId(idContato)
+            contato?.let {
+                with(it) {
+                    _uiState.value = _uiState.value.copy(
+                        id = id,
+                        nome = nome,
+                        sobrenome = sobrenome,
+                        aniversario = aniversario,
+                        telefone = telefone,
+                        fotoPerfil = fotoPerfil
+                    )
+                }
+
+            }
+
+        }
     }
 
     suspend fun removeContato() {
