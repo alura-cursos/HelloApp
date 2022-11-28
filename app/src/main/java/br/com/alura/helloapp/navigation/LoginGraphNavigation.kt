d0752dc15dc91b59eaaa8a28a74d85d65b355eb1
Junior <35709152+git-jr@users.noreply.github.com>
2022-11-28 11:50:53 -0300

    4.3

--- app/src/main/java/br/com/alura/helloapp/navigation/LoginGraphNavigation.kt
+++ app/src/main/java/br/com/alura/helloapp/navigation/LoginGraphNavigation.kt
@@ -3,17 +3,25 @@ package br.com.alura.helloapp.navigation
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.platform.LocalContext
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
 import br.com.alura.helloapp.DestinosHelloApp
+import br.com.alura.helloapp.preferences.dataStore
 import br.com.alura.helloapp.ui.login.FormularioLoginTela
 import br.com.alura.helloapp.ui.login.FormularioLoginViewModel
 import br.com.alura.helloapp.ui.login.LoginTela
 import br.com.alura.helloapp.ui.login.LoginViewModel
 import br.com.alura.helloapp.ui.navegaLimpo
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 fun NavGraphBuilder.loginGraph(
     navController: NavHostController
@@ -34,15 +42,34 @@ fun NavGraphBuilder.loginGraph(
                 }
             }
 
+            val dataStore = LocalContext.current.dataStore
+            val coroutineScope = rememberCoroutineScope()
+
             LoginTela(
                 state = state,
                 onClickLogar = {
+                    coroutineScope.launch {
+                        dataStore.edit { preferences ->
+                            preferences[booleanPreferencesKey("logado")] = true
+                        }
+                    }
                     viewModel.tentaLogar()
                 },
                 onClickCriarLogin = {
                     navController.navigate(DestinosHelloApp.FormularioLogin.rota)
                 }
             )
+
+            LaunchedEffect(Unit) {
+                coroutineScope.launch {
+                    dataStore.data.collect { preferences ->
+                        val logado = preferences[booleanPreferencesKey("logado")]
+                        if (logado == true) {
+                            navController.navegaLimpo(DestinosHelloApp.HomeGraph.rota)
+                        }
+                    }
+                }
+            }
         }
 
         composable(
